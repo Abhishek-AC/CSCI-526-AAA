3209c97e4e2472d8e1ad2fbe3e9380c40e77d85b
Beiyou Zhu <beiyouzh@usc.edu>
2020-06-10 01:46:46 -0700

    Code complete for the scene
    More comments are comming

--- Assets/Scripts/ClickToRotate.cs
+++ Assets/Scripts/ClickToRotate.cs
@@ -11,7 +11,7 @@ public class ClickToRotate : MonoBehaviour
     private bool isRotating;
     private float maxAnglesPerClick;
     private float currentAngleDegree;
-    // Start is called before the first frame update
+    // Start is called to initialize data
     void Start()
     {
         isRotatable = true;
@@ -22,18 +22,36 @@ public class ClickToRotate : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //check if the rotation gear is clicked
         if (Input.GetMouseButtonDown(0) && isRotatable)
         {
-            isRotating = true;
-            isRotatable = false;
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit))
+            {
+                if (hit.transform.name == "RotationGear")
+                {
+                    isRotating = true;
+                    isRotatable = false;
+                }
+            }
         }
+        //if the current status is rotating, rotate the cube group in specified rotating speed
         if (isRotating)
         {
             Vector3 angleSpeed = new Vector3(0, 0, m_angleSpeed);
             currentAngleDegree += m_angleSpeed*Time.deltaTime;
-            m_rotationCubeGroup.transform.Rotate(angleSpeed*Time.deltaTime, Space.World);
-            if (currentAngleDegree >= maxAnglesPerClick)
+            //only rotate when the angle that has been rotated is less than the maximum rotating degree per click
+            if(currentAngleDegree < maxAnglesPerClick)
             {
+                m_rotationCubeGroup.transform.Rotate(angleSpeed * Time.deltaTime, Space.World);
+                float remaingDegree = maxAnglesPerClick - currentAngleDegree;
+                if (remaingDegree <= m_angleSpeed * Time.deltaTime)
+                {
+                    m_rotationCubeGroup.transform.Rotate(new Vector3(0,0,remaingDegree), Space.World);
+                }
+            }
+            else { 
                 isRotatable = true;
                 isRotating = false;
                 currentAngleDegree = 0f;
