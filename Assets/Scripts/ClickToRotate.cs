ee5af22bbb0dad8f42a198b20356cd1e366136c6
Beiyou Zhu <beiyouzh@usc.edu>
2020-06-10 01:29:19 -0700

    Finish the scene
    Will add comments on next commit

---
+++ Assets/Scripts/ClickToRotate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClickToRotate : MonoBehaviour
+{
+    public GameObject m_rotationCubeGroup;
+    public float m_angleSpeed;
+    private bool isRotatable;
+    private bool isRotating;
+    private float maxAnglesPerClick;
+    private float currentAngleDegree;
+    // Start is called before the first frame update
+    void Start()
+    {
+        isRotatable = true;
+        maxAnglesPerClick = 90f;
+        currentAngleDegree = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0) && isRotatable)
+        {
+            isRotating = true;
+            isRotatable = false;
+        }
+        if (isRotating)
+        {
+            Vector3 angleSpeed = new Vector3(0, 0, m_angleSpeed);
+            currentAngleDegree += m_angleSpeed*Time.deltaTime;
+            m_rotationCubeGroup.transform.Rotate(angleSpeed*Time.deltaTime, Space.World);
+            if (currentAngleDegree >= maxAnglesPerClick)
+            {
+                isRotatable = true;
+                isRotating = false;
+                currentAngleDegree = 0f;
+            }
+        }
+    }
+}
