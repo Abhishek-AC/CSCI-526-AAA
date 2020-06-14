0c81550b9b5e29b41fdbbe2a303aefec0495e09d
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-06-13 20:44:13 -0700

    update: fix for rotatation and then movement

--- Assets/Scripts/Walkable.cs
+++ Assets/Scripts/Walkable.cs
@@ -10,9 +10,26 @@ public class Walkable : MonoBehaviour
     public List<GamePath> possiblePath = new List<GamePath>();
     public Transform previousBlock;
     public float walkPointOffset = 1f;
+
+    public bool canWalkOnThisBlock = true;
     //get walkPoint of current cube
     public Vector3 GetWalkPoint()
     {
+        GameObject rotateObject = GameObject.Find("Rotate");
+        foreach(Transform t in rotateObject.transform){
+            t.gameObject.GetComponent<Walkable>().canWalkOnThisBlock = true;
+        }
+        if(transform.tag == "rotatableCube"){
+            GameObject capsuleObject = GameObject.Find("Player");
+            
+            // RaycastHit isCubeAbove;
+            if (Physics.Raycast(transform.position, capsuleObject.transform.up, 10f)) {
+                
+                this.canWalkOnThisBlock = false;
+                return new Vector3(0, -0.5f, 0);
+            }
+            return transform.position + capsuleObject.transform.up * (1 - walkPointOffset);    
+        }
         return transform.position + transform.up * (1 - walkPointOffset);
     }
     //draw gismos sphere to show the walk path
