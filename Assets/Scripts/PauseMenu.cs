cd5cd2cea3ab66bd53523f9c4cb78bcebbdcf521
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-06-18 13:36:49 -0700

    add: screen and button for PauseMenu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    public GameObject Pause_Menu, PauseButton, Level1;
+
+    public void Pause() {
+        Level1.SetActive(false);
+        Pause_Menu.SetActive(true);
+        PauseButton.SetActive(false);
+        
+    }
+
+    public void Resume() {
+        Level1.SetActive(true);
+        Pause_Menu.SetActive(false);
+        PauseButton.SetActive(true);
+        
+    }
+
+    public void Quit() {
+        Debug.Log("Game End in Build Mode");
+        Application.Quit();
+    }
+
+
+
+}
