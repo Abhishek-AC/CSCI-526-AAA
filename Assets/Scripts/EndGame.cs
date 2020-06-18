affd55979c4759834c4887fe524f7665a4e25747
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-06-18 13:37:30 -0700

    add: EndScreen

---
+++ Assets/Scripts/EndGame.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndGame : MonoBehaviour
+{
+    public void Quit () {
+        Debug.Log("End Game in Build Mode");
+        Application.Quit();
+    }
+    public void Relay() {
+        Debug.Log("Replay");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+    }
+}
