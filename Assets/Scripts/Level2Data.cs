6c67896ff6e6ab939773eafe554b76270b6b0446
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-07-01 01:24:49 -0700

    add: level2 serializable class

---
+++ Assets/Scripts/Level2Data.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Level2Data
+{
+    public float [] capsulePosition;
+    public bool isCollectedCrystal;
+    public float rotateGameObjectRotationZValue;
+    public float ferryGameObjectPositionXValue;
+    public float makePathCubePositionYValue;
+
+    public Level2Data (Level2 level2 ) {
+        capsulePosition = level2.capsulePosition;
+        isCollectedCrystal = level2.isCollectedCrystal;
+        rotateGameObjectRotationZValue = level2.rotateGameObjectRotationZValue;
+        ferryGameObjectPositionXValue = level2.ferryGameObjectPositionXValue;
+        makePathCubePositionYValue = level2.makePathCubePositionYValue;
+    }
+
+}
