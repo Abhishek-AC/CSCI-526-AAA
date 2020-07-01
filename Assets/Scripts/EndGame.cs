0a94c47fce38cf73f20ace7dfffed401a32613ff
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-07-01 01:27:19 -0700

    update: clear the file state when quit or replay

--- Assets/Scripts/EndGame.cs
+++ Assets/Scripts/EndGame.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
-
+using System.IO;
 public class EndGame : MonoBehaviour
 {
-    public void Quit () {
+    public void Quit()
+    {
         Debug.Log("End Game in Build Mode");
+        DeleteFile();
         Application.Quit();
     }
-    public void Relay() {
+    public void Relay()
+    {
         Debug.Log("Replay");
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex -1);
+        DeleteFile();
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+    void DeleteFile()
+    {
+        string path = Application.persistentDataPath + "/level2Data";
+        // check if file exists
+        if (!File.Exists(path))
+        {
+            Debug.Log("Save File Not Found in " + path);
+        }
+        else
+        {
+            Debug.Log("Deleting the state");
+            File.Delete(path);
+            RefreshEditorProjectWindow();
+        }
+    }
+    void RefreshEditorProjectWindow()
+    {
+#if UNITY_EDITOR
+        UnityEditor.AssetDatabase.Refresh();
+#endif
     }
 }
