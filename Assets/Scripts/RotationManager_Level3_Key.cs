e9711b5b394f8bb9d48a69be3d8b3e9c97bf12c1
simranthomas <simranth@usc.edu>
2020-07-04 20:08:44 -0700

    adjusted possible paths
    adjusted the possible paths as per rotation for RotationKey object

--- Assets/Scripts/RotationManager_Level3_Key.cs
+++ Assets/Scripts/RotationManager_Level3_Key.cs
@@ -6,15 +6,15 @@ public class RotationManager_Level3_Key : MonoBehaviour
     void Update()
     {
         //the following code connects cubes after rotation has been made
-        Transform cubeToConnect1 = transform.Find("Cube (64)");
-        Transform cubeToConnect2 = GameObject.Find("Cube (30)").transform;
+        Transform cubeToConnect1 = transform.Find("Cube (68)");
+        Transform cubeToConnect2 = GameObject.Find("Cube (80)").transform;
         //return if Transform not found
         if (cubeToConnect1 == null || cubeToConnect2 == null)
         {
             Debug.Log("Cube not reachable");
             return;
         }
-        //check if the rotation angle is 270 degree, which connects two b   locks
+        //check if the rotation angle is 270 degree, which connects two blocks
         if (Mathf.Abs(transform.rotation.eulerAngles.z - 0f) < 0.1f)
         {
             cubeToConnect1.GetComponent<Walkable>().possiblePath[0].active = true;
