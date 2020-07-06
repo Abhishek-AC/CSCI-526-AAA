0e803d02baf677d03d5967da06a23468ccdc3659
simranthomas <simranth@usc.edu>
2020-07-05 22:32:47 -0700

    fixed possible path bugs

--- Assets/Scripts/RotationManager_Level3_Dest.cs
+++ Assets/Scripts/RotationManager_Level3_Dest.cs
@@ -6,7 +6,7 @@ public class RotationManager_Level3_Dest : MonoBehaviour
     void Update()
     {
         //the following code connects cubes after rotation has been made
-        Transform cubeToConnect1 = transform.Find("Cube (54)");
+        Transform cubeToConnect1 = transform.Find("Cube (90)");
         Transform cubeToConnect2 = GameObject.Find("Stairs").transform;
         //return if Transform not found
         if (cubeToConnect1 == null || cubeToConnect2 == null)
@@ -17,13 +17,13 @@ public class RotationManager_Level3_Dest : MonoBehaviour
         //check if the rotation angle is 270 degree, which connects two blocks
         if (Mathf.Abs(transform.rotation.eulerAngles.x - 90f) < 0.1f)
         {
-            cubeToConnect1.GetComponent<Walkable>().possiblePath[0].active = true;
+            cubeToConnect1.GetComponent<Walkable>().possiblePath[1].active = true;
             cubeToConnect2.GetComponent<Walkable>().possiblePath[1].active = true;
             cubeToConnect2.GetComponent<Walkable>().canWalkOnThisBlock = true;
         }
         else
         {
-            cubeToConnect1.GetComponent<Walkable>().possiblePath[0].active = false;
+            cubeToConnect1.GetComponent<Walkable>().possiblePath[1].active = false;
             cubeToConnect2.GetComponent<Walkable>().possiblePath[1].active = false;
             cubeToConnect2.GetComponent<Walkable>().canWalkOnThisBlock = false;
         }
