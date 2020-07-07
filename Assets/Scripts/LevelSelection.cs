613a158d8adca60ba19f40a8a35b22e427f85fc8
Abhishek Chhabra <02abhishek.chhabra@gmail.com>
2020-07-07 14:42:42 -0700

    Added lock-unlock feature to LevelSelection scene

--- Assets/Scripts/LevelSelection.cs
+++ Assets/Scripts/LevelSelection.cs
@@ -1,9 +1,37 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class LevelSelection : MonoBehaviour
 {
+    public Button levelTwoButton, levelThreeButton;
+    int levelPassed;
+    void Start() {
+        levelPassed = PlayerPrefs.GetInt("LevelPassed");
+        levelTwoButton.interactable = false;
+        levelThreeButton.interactable = false;
+        
+        switch(levelPassed) {
+            case 2: 
+                levelTwoButton.interactable = true;
+                break;
+            case 4:
+                levelTwoButton.interactable = true;
+                levelThreeButton.interactable = true;
+                break;
+            default:
+                Debug.Log("Not on a level scene");
+                break;
+        }
+    }
+    
     public void levelToLoad(int level) {
         SceneManager.LoadScene(level);
+    }
+
+    public void resetPlayerPrefs() {
+        levelTwoButton.interactable = false;
+        levelThreeButton.interactable = false;
+        PlayerPrefs.DeleteAll();
     }
 }
