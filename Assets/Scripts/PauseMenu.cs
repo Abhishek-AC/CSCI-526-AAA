02acf51a839d14e98612edc99762b1055fe05e0f
simranthomas <simranth@usc.edu>
2020-07-13 22:38:19 -0700

    Changed Instructions Screen for Level 2
    Added instructions screen as an overlay for the info button as the save restore logic was not working

--- Assets/Scripts/PauseMenu.cs
+++ Assets/Scripts/PauseMenu.cs
@@ -4,7 +4,7 @@ using System.Collections; //SFX
 
 public class PauseMenu : MonoBehaviour
 {
-    public GameObject Pause_Menu, PauseButton, Level, InfoButton, Shaft_with_spokes;
+    public GameObject Pause_Menu, PauseButton, Level, InfoButton, Shaft_with_spokes, InstructionScreen, LevelBackground;
 
     public void Pause()
     {
@@ -97,6 +97,44 @@ public class PauseMenu : MonoBehaviour
         audio.Play();
         StartCoroutine(LoadNewScene());
         //SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void InfoLevel2Selected()
+    {
+        AudioSource audio = gameObject.GetComponent<AudioSource>();
+        audio.Play();
+        StartCoroutine(Wait());
+
+        Level.SetActive(false);
+        if (Shaft_with_spokes)
+            Shaft_with_spokes.SetActive(false);
+        InfoButton.SetActive(false);
+        Pause_Menu.SetActive(false);
+        PauseButton.SetActive(false);
+        if (InstructionScreen)
+        {
+            InstructionScreen.SetActive(true);
+            LevelBackground.SetActive(false);
+        }
+    }
+    public void InfoLevel2Unselected()
+    {
+        AudioSource audio = gameObject.GetComponent<AudioSource>();
+        audio.Play();
+        StartCoroutine(Wait());
+
+        Level.SetActive(true);
+        if (Shaft_with_spokes)
+            Shaft_with_spokes.SetActive(true);
+        InfoButton.SetActive(true);
+        Pause_Menu.SetActive(false);
+        PauseButton.SetActive(true);
+        if (InstructionScreen)
+        {
+            InstructionScreen.SetActive(false);
+            LevelBackground.SetActive(true);
+        }
+
     }
 
     //SFX++
